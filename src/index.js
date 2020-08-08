6cebaf6814026c2a543f1c11bc6cb4f11a7d37f1
miho yamagata <miho.yamagata@gmail.com>
2020-08-08 17:34:56 +0900

    added App class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './assets/scss/styles.scss';
+
+class App {
+	constructor() {
+		this.workLength = 25;
+		this.breakLength = 5;
+		this.isTimerStopped = true;
+		this.onWork = true;
+	}
+}
+
+window.addEventListener('load', () => new App());
