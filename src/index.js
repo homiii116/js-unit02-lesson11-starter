4cb84a1d5556534a348616b8fdb4000bdb78cc8f
miho yamagata <miho.yamagata@gmail.com>
2020-08-08 22:49:31 +0900

    edited index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,36 @@ class App {
 		this.breakLength = 5;
 		this.isTimerStopped = true;
 		this.onWork = true;
+		this.startAt = null;
+		this.endAt = null;
 		this.timeDisplay = document.getElementById('time-display');
+		this.startButton = document.getElementById('start-button');
+		this.stopButton = document.getElementById('stop-button');
+		this.displayTime();
+	}
+
+	startTimer() {}
+	updateTimer(){}
+
+	displayTime() {
+		let mins;
+		let secs;
+		if (this.isTimerStopped) {
+			mins = this.workLength;
+			secs = 0;
+		}
+	
+		const minsString = mins.toString();
+		let secsString = secs.toString();
+	
+		if (secs < 10) {
+			secsString = `0${secsString}`;
+		}
+	
+		this.timeDisplay.innerHTML = `${minsString}:${secsString}`;
 	}
 }
+
 
 export default App;
 
