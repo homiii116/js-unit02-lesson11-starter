ba42c6bfd83bd574d28883a60672136cf8c31055
miho yamagata <miho.yamagata@gmail.com>
2020-08-10 22:25:34 +0900

    installed babel, did until 4-2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,60 @@
 import './assets/scss/styles.scss';
+import moment from 'moment';
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const DAY = 24 * 60 * MINUTE;
 
 class App {
 	constructor() {
-		this.workLength = 25;
-		this.breakLength = 5;
-		this.isTimerStopped = true;
-		this.onWork = true;
-		this.startAt = null;
-		this.endAt = null;
+	this.workLength = 25;
+	this.breakLength = 5;
+	this.isTimerStopped = true;
+	this.onWork = true;
+	this.startAt = null; //カウントダウン開始時の時間
+	this.endAt = null; //カウントダウン終了時の時間
+	this.getElements();
+	this.toggleEvents();
+	this.startTimer = this.startTimer.bind(this);
+	this.updateTimer = this.updateTimer.bind(this);
+	this.displayTime = this.displayTime.bind(this);
+	}
+
+	getElements() {
 		this.timeDisplay = document.getElementById('time-display');
 		this.startButton = document.getElementById('start-button');
 		this.stopButton = document.getElementById('stop-button');
-		this.displayTime();
 	}
 
-	startTimer() {}
-	updateTimer(){}
+	toggleEvents() {
+		this.startButton.addEventListener('click', this.startTimer);
+	}
 
-	displayTime() {
+	startTimer(e = null, time = moment()) {
+		if (e)e.preventDefault();
+		this.startButton.disabled = true;
+		this.stopButton.disabled = false;
+		this.isTimerStopped = false;
+		this.startAt = time;
+		const startAtClone = moment(this.startAt);
+		this.endAt = startAtClone.add(this.workLength, 'minutes');
+		this.timerUpdater = window.setInterval(this.updateTimer, 500);
+	}
+
+	updateTimer(time = moment()) {
+		this.diaplayTime(time);
+	}
+
+	displayTime(time = moment()) {
 		let mins;
 		let secs;
 		if (this.isTimerStopped) {
-			mins = this.workLength;
+			mins = this.workLength.toString();
 			secs = 0;
+		} else {
+			const diff = this.endAt.diff(time);
+			mins = Math.floor(diff / MINUTE);
+			secs = Math.floor((diff % MINUTE) / 1000);
 		}
 	
 		const minsString = mins.toString();
@@ -35,7 +67,6 @@ class App {
 		this.timeDisplay.innerHTML = `${minsString}:${secsString}`;
 	}
 }
-
 
 export default App;
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 import App from '../src/index';
 import template from './template';
+import moment from 'moment';
 
 describe('displayTime', () => {
 	test('初期化時に25:00を表示する。', () => {
@@ -9,4 +10,41 @@ describe('displayTime', () => {
 		expect(app.isTimerStopped).toBeTruthy();
 		expect(timeDisplay.innerHTML).toEqual('25:00');
 	});
+	
+	test('カウントダウン中の時間を適切に表示する。', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		const now = moment();
+		const startOfToday = now.startOf('day');
+		app.startButton.disabled = true;
+		app.stopButton.disabled = false;
+		app.isTimerStopped = false;
+		app.startAt = startOfToday;
+		app.endAt = moment(startOfToday).add(25, 'minutes');
+		app.displayTime(moment(startOfToday).add(51, 'seconds'));
+		const timeDisplay = document.getElementById('time-display');
+		expect(timeDisplay.innerHTML).toEqual('24:09');
+	})
+});
+
+describe('startTimer', () => {
+	test('スタートボタンにdisable属性を追加', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		app.startTimer();
+		const startButton = document.getElementById('start-button');
+		const stopButton = document.getElementById('stop-button');
+		expect(startButton.disabled).toEqual(true);
+		expect(stopButton.disabled).toEqual(false);
+		expect(app.isTimerStopped).toEqual(false);
+	});
+
+	test('startAtとendAtを適切に設定する。', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		const now = moment();
+		app.startTimer(null, now);
+		expect(app.startAt.valueOf()).toEqual(now.valueOf());
+		expect(app.endAt.valueOf()).toEqual(now.add(25, 'minutes').valueOf());
+	})
 });
