cfcd3726ccb085a0d19c5d4b0e47903a89c7d141
miho yamagata <miho.yamagata@gmail.com>
2020-08-16 17:39:35 +0900

    doing 6-4

--- src/index.js
+++ src/index.js
@@ -7,28 +7,32 @@ const DAY = 24 * 60 * MINUTE;
 
 class App {
 	constructor() {
-	this.workLength = 25;
-	this.breakLength = 5;
-	this.isTimerStopped = true;
-	this.onWork = true;
-	this.startAt = null; //カウントダウン開始時の時間
-	this.endAt = null; //カウントダウン終了時の時間
 	this.startTimer = this.startTimer.bind(this);
 	this.updateTimer = this.updateTimer.bind(this);
+	this.stopTimer = this.stopTimer.bind(this);
+	this.resetValues = this.resetValues.bind(this);
 	this.displayTime = this.displayTime.bind(this);
+	this.getHistory = App.getHistory.bind(this);
+	this.saveIntervalData = this.saveIntervalData.bind(this);
+	this.displayCyclesToday = this.displayCyclesToday.bind(this);
+	this.resetValues();
 	this.getElements();
 	this.toggleEvents();
 	this.displayTime();
+	this.displayCyclesToday();
 	}
 
 	getElements() {
 		this.timeDisplay = document.getElementById('time-display');
 		this.startButton = document.getElementById('start-button');
 		this.stopButton = document.getElementById('stop-button');
+		this.countOfTodayDisplay = document.getElementById('count-today'); // 回数の表示
+		this.percentOfTodayDisplay = document.getElementById('percent-today'); // ％の表示
 	}
 
 	toggleEvents() {
 		this.startButton.addEventListener('click', this.startTimer);
+		this.stopButton.addEventListener('click', this.stopTimer);
 	}
 
 	startTimer(e = null, time = moment()) {
@@ -46,12 +50,35 @@ class App {
 	updateTimer(time = moment()) {
 		const rest = this.endAt.diff(time); //残り時間を取得
 		if (rest <= 0) {
+			if (this.onWork) {
+				this.saveIntervalData(time);  // 作業時からの切り替り時のみsaveIntervalを呼び出す。
+			}
 			this.onWork = !this.onWork;
 			this.startAt = time;
 			this.endAt = this.onWork ? moment(time).add(this.workLength, 'minutes')
 			 : moment(time).add(this.breakLength, 'minutes');
 		}
 		this.displayTime(time);
+		console.log(this.endAt);
+	}
+
+	stopTimer(e = null) {
+		if (e)e.preventDefault();
+		this.resetValues();
+		this.startButton.disabled = false;
+		this.stopButton.disabled = true;
+		window.clearInterval(this.timerUpdater);
+		this.timerUpdater = null;
+		this.displayTime();
+	}
+
+	resetValues() {
+		this.workLength = 25;
+		this.breakLength = 5;
+		this.startAt = null;
+		this.endAt = null;
+		this.isTimerStopped = true;
+		this.onWork = true;
 	}
 
 	displayTime(time = moment()) {
@@ -65,15 +92,40 @@ class App {
 			mins = Math.floor(diff / MINUTE);
 			secs = Math.floor((diff % MINUTE) / 1000);
 		}
-	
 		const minsString = mins.toString();
 		let secsString = secs.toString();
-	
 		if (secs < 10) {
 			secsString = `0${secsString}`;
 		}
-	
 		this.timeDisplay.innerHTML = `${minsString}:${secsString}`;
+	}	
+	
+	static getHistory() {
+		const items = localStorage.getItem('intervalData');
+		let collection = [];
+		// localStorageにはArrayを直接保存出来ないので、JSON形式で保存。
+    // 取り出す時は、JSON.parseでarrayに戻す。
+		if (items) collection = JSON.parse(items);
+		return collection;
+	}
+
+	saveIntervalData(momentItem) {
+		const collection = this.getHistory();  // 既に保存されているデータの取得。
+		collection.push(momentItem.valueOf()); // 新しいデータを追加する。
+		localStorage.setItem('intervalData', JSON.stringify(collection)); // JSON形式で再度保存する。
+	}
+
+	displayCyclesToday(time = moment()) {
+		const collection = this.getHistory();
+		const startOfToday = time.startOf('day');
+		// 今日の始まりより後の時間のデータのみを取得してfilterItemsに格納する。
+		const filterItems = collection.filter(item => (
+			parseInt(item, 10) >= startOfToday.valueOf()
+		));
+		const count = filterItems.length;
+		const percent = count / 4 * 100;
+		this.countOfTodayDisplay.innerHTML = `${count.toString()}回 / 4回`;
+		this.percentOfTodayDisplay.innerHTML = `目標を${percent}%達成中です。`;
 	}
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -66,6 +66,7 @@ describe('updateTimer', () => {
 		const timeDisplay = document.getElementById('time-display');
 		expect(timeDisplay.innerHTML).toEqual('5:00');
 		expect(app.onWork).not.toBeTruthy(); // 休憩時間に切り替わっている。
+		expect(app.getHistory()).toEqual([endAt.add(100, 'millisecond').valueOf()]); // データの保存を確認
 	});
 	
 	test('休憩時間が終わったら作業時間に切り替える。', () => {
@@ -85,5 +86,70 @@ describe('updateTimer', () => {
 		const timeDisplay = document.getElementById('time-display');
 		expect(timeDisplay.innerHTML).toEqual('25:00');
 		expect(app.onWork).toBeTruthy(); // 作用時間に切り替わっている。
+	})
+})
+
+describe('stopTimer', () => {
+	test('it should reset the timer', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		const now = moment();
+		const startOfToday = now.startOf('day');
+		app.startButton.disabled = true;
+		app.stopButton.disabled = false;
+		app.isTimerStopped = false;
+		app.startAt = startOfToday;
+		app.endAt = moment(now).add(20, 'minutes');
+		app.stopTimer();
+		const timeDisplay = document.getElementById('time-display');
+		expect(timeDisplay.innerHTML).toEqual('25:00');
+		expect(app.onWork).toBeTruthy();
+		expect(app.isTimerStopped).toBeTruthy();
+		expect(app.startButton.disabled).not.toBeTruthy();
+	});
+});
+
+describe('App.getHistory', () => {
+	test('終了した作業インターバルの終了時間一覧を取得する。', () => {
+		const startOfToday = moment().startOf('day');
+		const val1 = moment(startOfToday).subtract(5, 'days').add(30, 'minutes').valueOf();
+		const val2 = moment(startOfToday).subtract(5, 'days').add(60, 'minutes').valueOf();
+		const collection = [val1, val2];
+		// intervalDataというkey名でデータを保存
+		localStorage.setItem('intervalData', JSON.stringify(collection));
+		expect(App.getHistory()).toContain(val1);
+		localStorage.clear();
+	})
+})
+
+describe('saveIntervalData', () => {
+	test('it should be save array of items', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		const startOfToday = moment().startOf('day');
+		// 作業終了時の時間のテストデータを作成
+		const item = moment(startOfToday).subtract(5, 'days').add(60, 'minutes');
+		app.saveIntervalData(item);
+		expect(JSON.parse(localStorage.getItem('intervalData'))).toContain(item.valueOf());
+		localStorage.clear();
+	});
+});
+
+describe('displayCyclesToday', () => {
+	test('当日の完了した作業サイクル数を表示する。', () => {
+		document.body.innerHTML = template;
+		const app = new App();
+		const startOfToday = moment().startOf('day');
+		const time = moment(startOfToday).add(5, 'hours'); //現在時刻を、午前5時に設定
+		const val1 = moment(startOfToday).add(30, 'minutes').valueOf();  // 午前0:30に作業完了
+		const val2 = moment(startOfToday).add(60, 'minutes').valueOf();  // 午前1:00に作業完了
+		const collection = [val1, val2];
+		localStorage.setItem('intervalData', JSON.stringify(collection));
+		app.displayCyclesToday(time);
+		const countToday = document.getElementById('count-today');
+		const percentToday = document.getElementById('percent-today');
+		expect(countToday.innerHTML).toEqual('2回 / 4回');
+		expect(percentToday.innerHTML).toEqual('目標を50%達成中です。');
+		localStorage.clear();
 	})
 })
